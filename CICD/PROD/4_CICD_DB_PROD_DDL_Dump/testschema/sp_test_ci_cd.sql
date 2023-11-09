c9c2d3be887d1cf5014ec2da18e4ec6088d0b75b
Lan Nguyen <lan.nguyen@postnl.nl>
2023-11-09 12:45:47 +0000

    lastest update on PROD after release

---
+++ CICD/PROD/4_CICD_DB_PROD_DDL_Dump/testschema/sp_test_ci_cd.sql
@@ -0,0 +1,10 @@
+create or replace procedure testschema.sp_test_ci_cd()
+	language plpgsql
+as $$
+BEGIN
+    RAISE INFO 'Add new changes';
+    insert into testschema.test_ci_cd values (2,4);
+    RAISE INFO 'Code change 1';
+END;
+$$;
+
