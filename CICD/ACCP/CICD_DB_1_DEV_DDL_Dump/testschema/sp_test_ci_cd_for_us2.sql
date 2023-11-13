d355caed6fb6d9a50d0a98e373c24a76a4eacca5
Lan Nguyen <lan.nguyen@postnl.nl>
2023-11-13 14:46:40 +0100

    initial commit sproc US2

---
+++ CICD/ACCP/CICD_DB_1_DEV_DDL_Dump/testschema/sp_test_ci_cd_for_us2.sql
@@ -0,0 +1,10 @@
+create or replace procedure testschema.sp_test_ci_cd_for_us2()
+	language plpgsql
+as $$
+BEGIN
+    RAISE INFO 'Add new changes';
+    insert into testschema.test_ci_cd values (2,4);
+
+END;
+$$;
+
